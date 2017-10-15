d3f96030b62ec859a4f64af1654ad20b2bc4d0e0
Vincent Zhang <vzhang@purdue.edu>
2017-10-15 17:23:06 -0400

    User store impl

--- src/main/java/com/divinitor/discord/wahrbot/core/store/impl/UserStoreImpl.java
+++ src/main/java/com/divinitor/discord/wahrbot/core/store/impl/UserStoreImpl.java
@@ -5,16 +5,14 @@ import com.divinitor.discord.wahrbot.core.util.gson.StandardGson;
 import com.divinitor.discord.wahrbot.core.util.inject.JedisProvider;
 import com.divinitor.discord.wahrbot.core.util.redis.RedisList;
 import com.divinitor.discord.wahrbot.core.util.redis.RedisMap;
+import com.divinitor.discord.wahrbot.core.util.redis.RedisSet;
 import com.google.inject.Inject;
 import net.dv8tion.jda.core.entities.User;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.ScanParams;
 import redis.clients.jedis.ScanResult;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.StringJoiner;
+import java.util.*;
 import java.util.function.BiFunction;
 import java.util.function.Function;
 
@@ -22,13 +20,23 @@ import java.util.function.Function;
 public class UserStoreImpl implements UserStore {
 
     private static final String BASE_KEY = "com.divinitor.discord.wahrbot.core.store.user";
+    private final User user;
     @Inject
     private JedisProvider provider;
-    private final User user;
 
 
     public UserStoreImpl(User user) {
         this.user = user;
+    }
+
+    private static String key(String... args) {
+        StringJoiner joiner = new StringJoiner(".");
+        joiner.add(BASE_KEY);
+        for (String arg : args) {
+            joiner.add(arg);
+        }
+
+        return joiner.toString();
     }
 
     @Override
@@ -62,12 +70,17 @@ public class UserStoreImpl implements UserStore {
 
     @Override
     public void put(String key, Object value) {
-        if (value instanceof Map) {
+        if (value instanceof String) {
+            this.put(key, (String) value);
+        } else if (value instanceof Map) {
             //  This will also store it in Redis so we're good
             new RedisMap(this.provider, key(key), StandardGson.instance(), value.getClass(), (Map) value);
         } else if (value instanceof List) {
             //  This will also store it in Redis so we're good
             new RedisList(this.provider, key(key), StandardGson.instance(), value.getClass(), (List) value);
+        } else if (value instanceof Set) {
+            //  This will also store it in Redis so we're good
+            new RedisSet(this.provider, key(key), StandardGson.instance(), value.getClass(), (Set) value);
         } else {
             this.put(key(key), this.serializer().apply(value));
         }
@@ -77,15 +90,15 @@ public class UserStoreImpl implements UserStore {
     public <T> T getObject(String key, Class<T> clazz) {
         if (clazz == String.class) {
             return (T) this.getString(key);
-        }
-
-        if (clazz.isAssignableFrom(Map.class)) {
+        } else if (clazz.isAssignableFrom(Map.class)) {
             return (T) new RedisMap<>(this.provider, key(key), StandardGson.instance(), clazz);
         } else if (clazz.isAssignableFrom(List.class)) {
             return (T) new RedisList<>(this.provider, key(key), StandardGson.instance(), clazz);
+        } else if (clazz.isAssignableFrom(Set.class)) {
+            return (T) new RedisSet<>(this.provider, key(key), StandardGson.instance(), clazz);
+        } else {
+            return this.deserializer(clazz).apply(this.getString(key), clazz);
         }
-
-        return this.deserializer(clazz).apply(this.getString(key), clazz);
     }
 
     @Override
@@ -103,15 +116,5 @@ public class UserStoreImpl implements UserStore {
     @Override
     public <T> Function<T, String> serializer() {
         return StandardGson.instance()::toJson;
-    }
-
-    private static String key(String... args) {
-        StringJoiner joiner = new StringJoiner(".");
-        joiner.add(BASE_KEY);
-        for (String arg : args) {
-            joiner.add(arg);
-        }
-
-        return joiner.toString();
     }
 }
