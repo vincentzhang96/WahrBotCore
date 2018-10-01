ff561fc1d6744fc9bb10f4860c1ecfcdcd5e12d1
Vincent Zhang <vzhang@purdue.edu>
2018-09-30 19:29:03 -0700

    Null/not found handling in dynconfig

--- src/main/java/com/divinitor/discord/wahrbot/core/config/dyn/DynConfigStore.java
+++ src/main/java/com/divinitor/discord/wahrbot/core/config/dyn/DynConfigStore.java
@@ -1,5 +1,6 @@
 package com.divinitor.discord.wahrbot.core.config.dyn;
 
+import java.util.NoSuchElementException;
 import java.util.function.BiFunction;
 import java.util.function.Function;
 
@@ -25,32 +26,139 @@ public interface DynConfigStore {
 
     String getString(String key);
 
+    default String getString(String key, String def) {
+        String val = this.getString(key);
+        if (val == null) {
+            return def;
+        }
+
+        return val;
+    }
+
     default long getLong(String key) {
-        return Long.parseLong(this.getString(key));
+        String val = this.getString(key);
+        if (val == null) {
+            throw new NoSuchElementException(key);
+        }
+
+        try {
+            return Long.parseLong(val);
+        } catch (NumberFormatException nfe) {
+            throw new IllegalArgumentException(key + " is not an integer");
+        }
+    }
+
+    default long getLong(String key, long def) {
+        try {
+            return this.getLong(key);
+        } catch (Exception e) {
+            return def;
+        }
     }
 
     default int getInt(String key) {
         return (int) getLong(key);
     }
 
+    default int getInt(String key, int def) {
+        try {
+            return this.getInt(key);
+        } catch (Exception e) {
+            return def;
+        }
+    }
+
     default double getDouble(String key) {
-        return Double.parseDouble(this.getString(key));
+        String val = this.getString(key);
+        if (val == null) {
+            throw new NoSuchElementException(key);
+        }
+
+        try {
+            return Double.parseDouble(val);
+        } catch (NumberFormatException nfe) {
+            throw new IllegalArgumentException(key + " is not a real");
+        }
+    }
+
+    default double getDouble(String key, double def) {
+        try {
+            return this.getDouble(key);
+        } catch (Exception e) {
+            return def;
+        }
     }
 
     default float getFloat(String key) {
         return (float) getDouble(key);
     }
 
+    default float getFloat(String key, float def) {
+        try {
+            return this.getFloat(key);
+        } catch (Exception e) {
+            return def;
+        }
+    }
+
     default boolean getBoolean(String key) {
-        return Boolean.parseBoolean(this.getString(key));
+        String val = this.getString(key);
+        if (val == null) {
+            throw new NoSuchElementException(key);
+        }
+
+        try {
+            return Boolean.parseBoolean(val);
+        } catch (Exception e) {
+            throw new IllegalArgumentException(key + " is not a boolean");
+        }
+    }
+
+    default boolean getBoolean(String key, boolean def) {
+        try {
+            return this.getBoolean(key);
+        } catch (Exception e) {
+            return def;
+        }
     }
 
     default <T> T getObject(String key, Class<T> clazz) {
-        return this.deserializer(clazz).apply(this.getString(key), clazz);
+        String val = this.getString(key);
+        if (val == null) {
+            return null;
+        }
+
+        return this.deserializer(clazz).apply(val, clazz);
+    }
+
+    default <T> T getObject(String key, Class<T> clazz, T def) {
+        try {
+            T ret = this.getObject(key, clazz);
+            if (ret == null) {
+                return def;
+            }
+
+            return ret;
+        } catch (Exception e) {
+            return def;
+        }
     }
 
     default <T, V> T getObject(String key, Class<T> clazz, Class<V> vClass) {
         return this.getObject(key, clazz);
+    }
+
+    default <T, V> T getObject(String key, Class<T> clazz, Class<V> vClass, T def) {
+        try {
+            T ret = this.getObject(key, clazz, vClass);
+            if (ret == null) {
+                return def;
+            }
+
+            return ret;
+        } catch (Exception e) {
+            return def;
+        }
     }
 
     default DynConfigHandle getStringHandle(String key) {
